994267fd261f1ad8f593d4fb6e7bb9e718d70d4f
celestian <petr.celestian@gmail.com>
2017-09-06 16:24:48 +0200

    Clients: Add athorized and unauthorized clients
    Resolves #5

--- zonkylla/zonky.py
+++ zonkylla/zonky.py
@@ -13,22 +13,48 @@ import pkg_resources
 from oauthlib.oauth2 import LegacyApplicationClient
 from requests_oauthlib import OAuth2Session
 from requests.auth import HTTPBasicAuth
+import requests
 
 
-class ABCClient(metaclass=ABCMeta):
+class AbstractClient(metaclass=ABCMeta):
     """Abstract class for Zonky clients"""
 
-    @abstractmethod
-    def _request(self, method, url, data):
-        raise NotImplementedError
+    def __init__(self, host):
+        self._host = host
+        self._headers = {
+            'Accept': 'application/json',
+            'Content-Type': 'application/json',
+            'User-Agent': self._user_agent,
+        }
 
     @property
     def _user_agent(self):
         return 'zonkylla/{} ({})'.format(pkg_resources.require('zonkylla')
                                          [0].version, 'https://github.com/celestian/zonkylla')
 
+    @abstractmethod
+    def get(self, url, data):
+        """GET Method"""
+        raise NotImplementedError
 
-class Client(ABCClient):  # pylint: disable=too-many-instance-attributes
+    @abstractmethod
+    def post(self, url, data):
+        """POST Method"""
+        raise NotImplementedError
+
+    @abstractmethod
+    def patch(self, url, data):
+        """PATCH Method"""
+        raise NotImplementedError
+
+    @abstractmethod
+    def delete(self, url, data):
+        """DELETE Method"""
+        raise NotImplementedError
+
+
+class OAuthClient(
+        AbstractClient):  # pylint: disable=too-many-instance-attributes
     """OAuth Client for Zonky"""
 
     def __init__(self, host, username, password):
@@ -37,16 +63,12 @@ class Client(ABCClient):  # pylint: disable=too-many-instance-attributes
         :param username: Username of Zonky user
         :param password: Password of Zonky user
         """
-        self._host = host
+
+        AbstractClient.__init__(self, host)
         self._client_id = 'web'
         self._client_secret = 'web'
         self._token_url = '{}/oauth/token'.format(self._host)
         self._scope = ['SCOPE_APP_WEB']
-        self._headers = {
-            'Accept': 'application/json',
-            'Content-Type': 'application/json',
-            'User-Agent': self._user_agent,
-        }
         self._token_headers = {
             'Accept': 'application/json',
             'Content-Type': 'application/x-www-form-urlencoded;charset=UTF-8',
@@ -88,7 +110,7 @@ class Client(ABCClient):  # pylint: disable=too-many-instance-attributes
     def _request(self, method, url, data=None):
         """Method for sending of request to Zonky
 
-        :param method: 'get',...
+        :param method: get, post, patch, delete
         :param url:
         :param data:
         :return:
@@ -101,13 +123,54 @@ class Client(ABCClient):  # pylint: disable=too-many-instance-attributes
             client_id=self._client_id,
             client_secret=self._client_secret)
 
+    def get(self, url, data=None):
+        return self._request('get', url, data).json()
+
+    def post(self, url, data=None):
+        raise NotImplementedError
+
+    def patch(self, url, data=None):
+        raise NotImplementedError
+
+    def delete(self, url, data=None):
+        raise NotImplementedError
+
     def get_wallet(self):
         """
 
         :return:
         """
         url = '{}/users/me/wallet'.format(self._host)
-        return self._request('get', url).json()
+        return self.get(url)
+
+
+class Client(AbstractClient):
+    """Client for Zonky"""
+
+    def __init__(self, host):
+        """
+
+        :param host:
+        """
+
+        AbstractClient.__init__(self, host)
+
+    def get(self, url, data=None):
+        return requests.get(url, data).json()
+
+    def post(self, url, data=None):
+        raise NotImplementedError
+
+    def patch(self, url, data=None):
+        raise NotImplementedError
+
+    def delete(self, url, data=None):
+        raise NotImplementedError
+
+    def get_loans(self):
+        """Get loans from Zonky"""
+        url = '{}/loans/marketplace'.format(self._host)
+        return self.get(url)
 
 
 class Zonky:
@@ -120,7 +183,8 @@ class Zonky:
         :param username:
         :param password:
         """
-        self._client = Client(host, username, password)
+        self._oauth_client = OAuthClient(host, username, password)
+        self._client = Client(host)
 
     def pretty_print(self, data):  # pylint: disable=no-self-use
         """
@@ -135,4 +199,5 @@ class Zonky:
 
         :return:
         """
-        self.pretty_print(self._client.get_wallet())
+        self.pretty_print(self._oauth_client.get_wallet())
+        self.pretty_print(self._client.get_loans())
