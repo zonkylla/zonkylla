4958697989de6b62110577e34e8c526c3c81ee92
Petr Blaho <petrblaho@gmail.com>
2017-10-08 14:10:29 +0200

    Refactors executing of sql into separate methods.
    Also adds logging to this method.

--- zonkylla/core/database.py
+++ zonkylla/core/database.py
@@ -5,6 +5,7 @@
 
 '''Database module'''
 
+import logging
 import sqlite3
 import yaml
 
@@ -17,6 +18,7 @@ class Database:
     def __init__(self):
         '''Init the connection'''
 
+        self.logger = logging.getLogger('zonkylla.Database')
         self.database = './zonkylla.db'
         self.connection = sqlite3.connect(self.database)
 
@@ -47,13 +49,37 @@ class Database:
             sql_commands.append(self._create_sql_cmd(table))
 
         for sql_command in sql_commands:
-            try:
-                with self.connection as con:
-                    con = con.cursor()
-                    con.execute(sql_command)
-            except sqlite3.Error as err:
-                print("sqlite3.Error occured: {}".format(err.args))
-                raise
+            self._execute(sql_command)
+
+    def _execute(self, sql, data=None):
+        """Executes SQL query with or without data"""
+        if data is None:
+            many = False
+        elif isinstance(data, list):
+            many = bool(all(isinstance(member, (tuple, list))
+                            for member in data))
+        else:
+            raise TypeError
+
+        try:
+            with self.connection as con:
+                con.row_factory = sqlite3.Row
+                con = con.cursor()
+                self.logger.debug("Executing '%s'", sql)
+                if many:
+                    self.logger.debug("with many data: '%s'", data)
+                    result = con.executemany(sql, data)
+                else:
+                    if data:
+                        self.logger.debug("with data: '%s'", data)
+                        result = con.execute(sql, data)
+                    else:
+                        result = con.execute(sql)
+            return result
+
+        except sqlite3.Error as err:
+            print("sqlite3.Error occured: {}".format(err.args))
+            raise
 
     def insert_transactions(self, transactions):
         '''Add transactions to the database'''
@@ -74,36 +100,27 @@ class Database:
                 item['orientation'],
                 item['transactionDate']))
 
-        try:
-            with self.connection as con:
-                con = con.cursor()
-                con.executemany('''
-                    INSERT OR REPLACE INTO Transactions(
-                        id,
-                        amount,
-                        category,
-                        customMessage,
-                        loanId,
-                        loanName,
-                        nickName,
-                        orientation,
-                        transactionDate
-                    ) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?)
-                ''', data)
-        except sqlite3.Error as err:
-            print("sqlite3.Error occured: {}".format(err.args))
-            raise
+        sql = '''
+            INSERT OR REPLACE INTO Transactions(
+                id,
+                amount,
+                category,
+                customMessage,
+                loanId,
+                loanName,
+                nickName,
+                orientation,
+                transactionDate
+            ) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?)
+        '''
+
+        self._execute(sql, data)
 
     def get_last_transaction_date(self):
         '''Get the datetime of last update'''
-        try:
-            with self.connection as con:
-                con = con.cursor()
-                con.execute('SELECT MAX(transactionDate) FROM Transactions')
-                dt_value = con.fetchone()[0]
-        except sqlite3.Error as err:
-            print("sqlite3.Error occured: {}".format(err.args))
-            raise
+        sql = 'SELECT MAX(transactionDate) FROM Transactions'
+        result = self._execute(sql).fetchone()
+        dt_value = result[0]
 
         return iso2datetime(dt_value) if dt_value else None
 
@@ -142,13 +159,7 @@ class Database:
 
         sql = 'INSERT OR REPLACE INTO Loans({}) VALUES ({})'.format(
             columns, placeholders)
-        try:
-            with self.connection as con:
-                con = con.cursor()
-                con.executemany(sql, rows)
-        except sqlite3.Error as err:
-            print("sqlite3.Error occured: {}".format(err.args))
-            raise
+        self._execute(sql, rows)
 
     def missing_loan_ids(self):
         '''Get loanId of loans which missing in Loans'''
@@ -161,15 +172,8 @@ class Database:
             AND Transactions.loanId IS NOT NULL
             GROUP BY Transactions.loanId;
         '''
-        try:
-            with self.connection as con:
-                con.row_factory = sqlite3.Row
-                con = con.cursor()
-                results = con.execute(sql).fetchall()
-                return list(map(lambda r: r['loanId'], results))
-        except sqlite3.Error as err:
-            print("sqlite3.Error occured: {}".format(err.args))
-            raise
+        results = self._execute(sql).fetchall()
+        return list(map(lambda r: r['loanId'], results))
 
     def insert_loan_investments(self, investments):
         '''Add investments of loan to the database'''
@@ -192,10 +196,4 @@ class Database:
 
         sql = 'INSERT OR REPLACE INTO LoanInvestments({}) VALUES ({})'.format(
             columns, placeholders)
-        try:
-            with self.connection as con:
-                con = con.cursor()
-                con.executemany(sql, rows)
-        except sqlite3.Error as err:
-            print("sqlite3.Error occured: {}".format(err.args))
-            raise
+        self._execute(sql, rows)
