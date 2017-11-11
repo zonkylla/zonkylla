8f95b44aaee959b2ab2222cceabee1d298a3c025
celestian <petr.celestian@gmail.com>
2017-11-11 07:09:32 +0100

    Add pep8-diff.sh
    label: feature
    
    We add wrapper on autopep8 which returns exit status:
    * 0 if there is no pep8 issue,
    * 1 if there is some pep8 issue.
    And the diff is printed.
    
    The tool autopep8 exits with status 0 in both cases.

---
+++ pep8-diff.sh
@@ -0,0 +1,19 @@
+#!/usr/bin/env bash
+
+# Obtain the path to the work directory
+RELATIVE_SOURCE_PATH=`dirname ${BASH_SOURCE[@]}`
+SOURCE_PATH=`readlink -f ${RELATIVE_SOURCE_PATH}`
+
+TMP_FILE=`mktemp`
+
+autopep8 --max-line-length=100 --diff -aaa -r ${SOURCE_PATH}/zonkylla/ > ${TMP_FILE}
+
+if [ -s ${TMP_FILE} ]
+then
+    cat ${TMP_FILE}
+    rm -f ${TMP_FILE}
+    exit 1
+else
+    rm -f ${TMP_FILE}
+    exit 0
+fi
