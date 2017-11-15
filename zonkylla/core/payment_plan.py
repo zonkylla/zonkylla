ea03946a2d406be1e544a7b69b7e2c3480fd8209
Dima Dorofieiev <dorofieiev.d@gmail.com>
2017-11-15 20:04:57 +0100

    payment_plan: Add payment_plan
    label: feature
    
    Models represent raw data in tables. This patch introduces the new way
    how to add logic of payment plan on top of models.

---
+++ zonkylla/core/payment_plan.py
@@ -0,0 +1,61 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+#
+# Copyright (C) 2017  zonkylla Contributors see COPYING for license
+
+'''Payment plan module'''
+
+
+class PaymentPlan():
+    '''Payment plan class'''
+
+    def __init__(self, amount, interest_rate, term_in_months):
+        self._amount = amount
+        self._interest_rate = interest_rate
+        self._term_in_months = term_in_months
+
+    @property
+    def amount(self):
+        '''Amount'''
+        return self._amount
+
+    @property
+    def interest_rate(self):
+        '''Interest rate'''
+        return self._interest_rate
+
+    @property
+    def term_in_months(self):
+        '''Term in months'''
+        return self._term_in_months
+
+    def period_to_pay_principal(self, step):
+        '''Period to pay principal'''
+        return self.monthly_payment * (1 / (self.monthly_interest) ** (self.term_in_months - step))
+
+    def period_to_pay_interest(self, step):
+        '''Period to pay interest'''
+        return self.monthly_payment * \
+            (1 - (1 / (self.monthly_interest) ** (self.term_in_months - step)))
+
+    @property
+    def payment_calendar(self):
+        '''Return payment calendar'''
+        return [{
+            'payment_number': i + 1,
+            'paid_total': (i + 1) * self.monthly_payment,
+            'period_to_pay_principal': self.period_to_pay_principal(i),
+            'period_to_pay_interest': self.period_to_pay_interest(i),
+        } for i in range(0, self.term_in_months + 1)]
+
+    @property
+    def monthly_interest(self):
+        '''Monthly interest'''
+        return 1 + (self.interest_rate / 12.0)
+
+    @property
+    def monthly_payment(self):
+        '''Return monthly payment'''
+        return self.amount * ((self.monthly_interest ** self.term_in_months) *
+                              (self.monthly_interest - 1)) / \
+            ((self.monthly_interest ** self.term_in_months) - 1)
