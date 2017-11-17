093e84c7e6fda44333159aff0056855d799a0aa2
celestian <petr.celestian@gmail.com>
2017-11-17 17:13:01 +0100

    config.py: Added singleton for configuration
    label: architecture

---
+++ zonkylla/core/config.py
@@ -0,0 +1,33 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+#
+# Copyright (C) 2017  zonkylla Contributors see COPYING for license
+
+'''Configuration module'''
+
+import configparser
+import logging
+
+from zonkylla.abstract.singleton_meta import Singleton
+
+
+class Config(metaclass=Singleton):
+    '''Configuration class'''
+
+    def __init__(self, **kwargs):
+        '''Init the configuration'''
+
+        self.logger = logging.getLogger('zonkylla.Abstract.Database')
+
+        self.config = configparser.ConfigParser()
+
+        readed = False
+        for name, value in kwargs.items():
+            if name == 'config_file':
+                self.config.read(value)
+                readed = True
+
+        if readed:
+            self.db_file = self.config['zonkylla']['db_file']
+        else:
+            self.db_file = None
